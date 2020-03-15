6e0ee890c18ff58c07f013fb01447763aa85f5a9
Thaddee Tyl <thaddee.tyl@gmail.com>
2020-03-15 08:04:22 +0100

    Add constants to algebraic analysis

---
+++ algebraic.js
@@ -0,0 +1,29 @@
+export class State {
+  // size: in bits.
+  constructor(size, bits = []) {
+    this.bits = new Array(size);
+    for (let i = 0; i < size; i++) {
+      this.bits[i] = bits[i] || new ConstantBit(0);
+    }
+  }
+}
+
+// Information sources:
+// Boolean Algebra: https://en.wikipedia.org/wiki/Boolean_algebra
+// Logic gates: https://en.wikipedia.org/wiki/Logic_gate
+
+class Bit {
+  constructor() {
+    this.type = this.constructor;
+  }
+}
+
+export class ConstantBit extends Bit {
+  // bit: 0 or 1.
+  constructor(bit) {
+    super();
+    this.set(bit);
+  }
+  set(bit) { this.value = (+bit)? 1: 0; return this; }
+  toString() { return String(this.value); }
+}
---
+++ test/algebraic.js
@@ -0,0 +1,9 @@
+//const assert = require('assert').strict;
+import {strict as assert} from 'assert';
+import {ConstantBit} from '../algebraic.js';
+
+// ConstantBit
+assert.equal(new ConstantBit(0).value, 0);
+assert.equal(new ConstantBit(1).value, 1);
+assert.equal(new ConstantBit(0).set(1).value, 1);
+assert.equal(new ConstantBit(1).set(0).value, 0);
